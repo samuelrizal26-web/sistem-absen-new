f83e263ea6779148f24e5a89d1661245071640de
LB Dev <dev@labalaba.com>
2026-07-07 22:04:13 +0700

    Phase 1: Capacitor setup complete - remove VitePWA, add Android platform

---
+++ frontend-web/android/app/src/main/java/com/labalaba/advertising/MainActivity.java
@@ -0,0 +1,5 @@
+package com.labalaba.advertising;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- frontend-web/vite.config.js
+++ frontend-web/vite.config.js
@@ -1,56 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['icon-192.png', 'icon-512.png'],
-      manifest: {
-        name: 'ONE_STOP CUTTING STICKER & ADVERTISING',
-        short_name: 'LB.ADV',
-        description: 'Aplikasi internal untuk absensi, stok, kasbon, dan cashflow Labalaba Advertising.',
-        theme_color: '#0A4D68',
-        background_color: '#EAFBFF',
-        display: 'standalone',
-        orientation: 'portrait',
-        icons: [
-          {
-            src: 'icon-192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'icon-512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-        ],
-      },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/.*/i,
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 100,
-                maxAgeSeconds: 60 * 60 * 24 // 24 hours
-              },
-              cacheableResponse: {
-                statuses: [0, 200]
-              }
-            }
-          }
-        ]
-      }
-    })
   ],
   server: {
     port: 5173,
